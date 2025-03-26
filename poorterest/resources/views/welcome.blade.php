846c2a9c285ff80a8d400ebbb7a52e5ce36dbdf7
Masha <maria_ib@yahoo.com>
2025-03-26 16:12:03 +0100

    Auth

--- poorterest/resources/views/welcome.blade.php
+++ poorterest/resources/views/welcome.blade.php
@@ -49,7 +49,7 @@
                 </nav>
             @endif
         </header>
-        <!-- ajouter bloc @include la nav avec le sign in/ sign up ici lol -->
+        <!-- ajouter bloc include la nav avec le sign in/ sign up ici lol -->
        <div class="grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
 
             <ul>
@@ -60,6 +60,7 @@
                         </a>
                     </li>
                 }
+                @endforeach
             </ul>
             
        </div>
